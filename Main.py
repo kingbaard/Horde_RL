6db855a1399f22e13753422957b196887732602e
kingbaard <bradley.rod.jensen@gmail.com>
2024-01-03 17:07:54 -0700

    implementing PyTorch - does not build

--- Main.py
+++ Main.py
@@ -1,12 +1,12 @@
 
 
-from environment import GameEnv
+from environment import HordeEnv
 from Agent import Agent
 
 import matplotlib.pyplot as plt
 
 agent = Agent(0.1, 0.9, 0.99)
-env = GameEnv('human')
+env = HordeEnv('human')
 
 minimum_epsilon = 0.01 
 decay_factor = 0.995
@@ -16,7 +16,7 @@ reward_history = []
 
 def main():
     for episode in range(1000):
-        # print(f"episode #{episode}")
+        print(f"episode #{episode}")
         state = env.reset()
         action = agent.choose_action(state)
         done = False
@@ -25,15 +25,17 @@ def main():
         reward_total = 0
         while not done:
             next_state, reward, done, _ = env.step(action)
-            # print(f'next_state: {next_state}')
-            next_action = agent.choose_action(next_state)
-            agent.learn(state, action, reward, next_state, next_action)
+
+            action = agent.choose_action_DQN(state)
+
+            # next_action = agent.choose_action(next_state)
+            # agent.learn(state, action, reward, next_state, next_action)
             reward_total += reward
             state = next_state
-            action = next_action
-
-            if agent.epsilon > minimum_epsilon:
-                agent.epsilon *= decay_factor
+            agent.memory.append((state, action, reward, next_state, done))
+            # if agent.epsilon > minimum_epsilon:
+            #     agent.epsilon *= decay_factor
+            agent.train_model()
 
         print(f"Reward: {reward_total}")
         reward_history.append(reward_total)
--- agent.py
+++ agent.py
@@ -1,19 +1,54 @@
 import numpy as np
+from envs.Constants import *
+import random
 
-# Create Q-table
-# Current Quad * 
-num_states = 71663616
-num_actions = 3 * 3 * 5
-Q = np.zeros((num_states, num_actions))
+import tensorflow as tf
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Dense
+from collections import deque
+
+from gym import spaces
 
 class Agent():
     def __init__(self, learning_rate, gamma, epsilon):
+        # self.observation_space = spaces.Box(low=0, high=3, shape=(60, 75), dtype=int )
         self.num_states = 71663616
         self.num_actions = 3 * 3 * 5
         self.learning_rate = learning_rate
         self.gamma = gamma
         self.epsilon = epsilon
-        self.Q = np.zeros((self.num_states, num_actions))
+        self.epsilon_decay = 0.995
+        self.epsilon_min = 0.1
+        self.batch_size = 64
+        self.memory = deque(maxlen=2000)
+        self.model = self.build_model()
+        self.Q = np.zeros((self.num_states, self.num_actions), dtype=np.int8)
+
+    def build_model(self):
+        model = Sequential()
+        model.add(Dense(24, input_dim=self.num_states, activation='relu'))
+        model.add(Dense(24, activation='relu'))
+        model.add(Dense(self.num_actions, activation='linear'))
+        model.compile(loss='mse', optimizer=tf.keras.optimizers.Adam(self.learning_rate))
+        return model
+    
+    def train_model(self):
+        if len(self.memory) < self.batch_size:
+            return
+        
+        minibatch = random.sample(self.memory, self.batch_size)
+        for state, action, reward, next_state, done in minibatch:
+            target = reward
+            if not done:
+                state_index = self.encode_state(state)
+                next_state_index = np.array([[self.encode_state(next_state)]])
+                target = (reward + self.gamma * np.amax(self.model.predict([next_state_index])))
+                target_f = self.model.predict(state_index)
+                target_f[0][action] = target
+                self.model.fit(state_index, target_f, epochs=1, verbose=0)
+            
+            if self.epsilon > self.epsilon_min:
+                self.epsilon *= self.epsilon_decay
 
     def observe(self, state):
         pass
@@ -23,17 +58,26 @@ class Agent():
         if np.random.rand() < self.epsilon:
             action_index = np.random.choice(self.num_actions)
         else:
-            encoded_state = self.encode_state(state)
-            action_index = np.argmax(self.Q[encoded_state])
+            action_index = np.argmax(self.Q[self.encode_state(state)])
+        return self.decode_action(action_index)
+    
+    def choose_action_DQN(self, state):
+        if np.random.rand() < self.epsilon:
+            action_index = np.random.choice(self.num_actions)
+        else:
+            encoded_action = self.encode_state(state)
+            print(encoded_action)
+            action_index = np.argmax(self.model.predict([encoded_action]))
         return self.decode_action(action_index)
     
     def learn(self, state, action, reward, next_state, next_action):
-        action_index = self.encode_action(action)
         state_index = self.encode_state(state)
-        next_action_index = self.encode_action(next_action)
+        action_index = self.encode_action(action)
         next_state_index = self.encode_state(next_state)
+        next_action_index = self.encode_action(next_action)
+
     
-        predict = self.Q[state_index , action_index]
+        predict = self.Q[state , action_index]
         target = reward + self.gamma * self.Q[next_state_index, next_action_index]
         self.Q[state_index, action_index] += self.learning_rate * (target - predict)
         
@@ -56,6 +100,31 @@ class Agent():
         hor_dir = index % 3
     
         return (hor_dir, ver_dir, shoot_dir)
+    
+    # def encode_state(self, state):
+    #     flattened_state = state.flatten()
+    #     index = 0
+
+    #     for i, val in enumerate(flattened_state):
+    #         index += val * (4 * i)
+
+    #     return index
+
+    # def decode_state(self, index):
+    #     shape = (WIDTH, HEIGHT)
+    #     base4_digits = []
+
+    #     while index > 0:
+    #         base4_digits.append(index % 4)
+    #         index = index // 4
+
+    #     if len(base4_digits) < WIDTH * HEIGHT:
+    #         base4_digits += [0] * (WIDTH * HEIGHT - len(base4_digits))
+
+    #     img_1d = np.array(base4_digits)
+    #     img_2d = img_1d.reshape(shape)
+        
+    #     return img_2d
 
     def encode_state(self, state):
         index = state[0]
